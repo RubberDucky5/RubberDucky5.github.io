23c20b24a3db7fd1d6908b6faa8c53acb5a8500b
RubberDuck55 <woodjb17@gmail.com>
2022-08-04 15:58:15 -0400

    Update index.js

--- emoji-palette/index.js
+++ emoji-palette/index.js
@@ -1,23 +1,23 @@
-let emoji;
+let emoji = {};
 
 fetch("data/emoji.json")
 .then(response => response.json()
 .then(data => emoji = data));
 
-let ea = emoji.emoji;
+let emojiList = emoji.emoji;
 let emojiButtonList = [];
 let container = document.getElementById("grid-container");
 
-for(let i = 0; i < ea.length; i++){
+for(let i = 0; i < emojiList.length; i++){
   let temp = document.createElement("button");
   temp.classList.add("copy-button");
-  temp.innerHTML = ea[i];
+  temp.innerHTML = emojiList[i];
   emojiButtonList.push(temp);
   temp.addEventListener('mousedown', (e) => {
     if(e.button != 0)
       return;
 
-    navigator.clipboard.writeText(ea[i]);
+    navigator.clipboard.writeText(emojiList[i]);
   });
   document.body.appendChild(emojiButtonList[i]);
 }
